22484495b2ce7f434949d5af024c1ad169f7e4d0
AndreaG <andrea.galletto@polito.it>
2021-01-04 23:04:39 +0100

    Aggiunto Activity LandingPage (solo layout (en/it) portrait/landscape), file dimension.xml per dimensioni uniformate, loghi, traduzione menu utente

---
+++ app/src/main/java/it/polito/s279941/libra/LandingPageActivity.kt
@@ -0,0 +1,11 @@
+package it.polito.s279941.libra
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LandingPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_landing_page)
+    }
+}
