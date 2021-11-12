42dac0cf33ef5a26be4719c69f3456ac324139ba
Roberta Filippini <roberta39@live.it>
2021-11-12 16:49:40 +0100

    utente profilo nome

--- app/src/main/java/it/polito/s279941/libra/utente/UtenteProfiloFragment.kt
+++ app/src/main/java/it/polito/s279941/libra/utente/UtenteProfiloFragment.kt
@@ -3,10 +3,19 @@ package it.polito.s279941.libra.utente
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import it.polito.s279941.libra.R
+import androidx.lifecycle.Observer
+import kotlinx.android.synthetic.main.utente_profilo_fragment.*
 
 class UtenteProfiloFragment: Fragment(R.layout.utente_profilo_fragment) {
+
+    private val utenteViewModel: UtenteViewModel by activityViewModels()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        user_name.text = utenteViewModel.utenteCorrente.nome
+        //user_surname.text = utenteViewModel.utenteCorrente.cognome
     }
 }
